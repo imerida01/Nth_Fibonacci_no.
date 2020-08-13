719e003893a68ea228b5d61c853bbd325576dfda
Kirandeep Kaur <kian16.ak@gmail.com>
2020-08-13 08:49:05 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cassert>
+
+// The following code calls a naive algorithm for computing a Fibonacci number.
+//
+// What to do:
+// 1. Compile the following code and run it on an input "40" to check that it is slow.
+//    You may also want to submit it to the grader to ensure that it gets the "time limit exceeded" message.
+// 2. Implement the fibonacci_fast procedure.
+// 3. Remove the line that prints the result of the naive algorithm, comment the lines reading the input,
+//    uncomment the line with a call to test_solution, compile the program, and run it.
+//    This will ensure that your efficient algorithm returns the same as the naive one for small values of n.
+// 4. If test_solution() reveals a bug in your implementation, debug it, fix it, and repeat step 3.
+// 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
+//    and submit it to the grader.
+
+long long fibonacci_naive(long long n) {
+    if (n <= 1)
+        return n;
+
+    return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
+}
+long long fibonacci_fast(long long n) {
+    if (n <= 1)
+        return n;
+    long long previous , current , next , i;
+    previous=0;
+    current=1;
+
+    for(i=2 ; i<=n ; i++)
+    {
+        next = previous + current;
+        previous = current;
+        current = next;
+    }
+
+    return current;
+}
+
+
+
+void test_solution() {
+    //assert(fibonacci_fast(3) == 2);
+    //assert(fibonacci_fast(10) == 55);
+    for (int n = 0; n < 90; ++n)
+        if(fibonacci_fast(n) == fibonacci_naive(n))
+        {
+            std::cout<<"\nat "<<n<<"testing"<<"\n";
+        }
+        else
+        {
+            std::cout<<"\n at "<<n<<"  "<<fibonacci_naive(n) <<" != "<<fibonacci_fast(n);
+            break;
+        }
+}
+
+int main() {
+    long long n = 0;
+    std::cin >> n;
+
+    std::cout << fibonacci_naive(n) << '\n';
+    std::cout<<"\n" << fibonacci_fast(n) << '\n';
+    test_solution();
+    return 0;
+}
---
+++ fibonacci_fast.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+long long fibonacci_fast(long long n) {
+    if (n <= 1)
+        return n;
+    long long previous , current , next , i;
+    previous=0;
+    current=1;
+
+    for(i=2 ; i<=n ; i++)
+    {
+        next = previous + current;
+        previous = current;
+        current = next;
+    }
+
+    return current;
+}
+
+
+ main() {
+    long long n = 0;
+    std::cin >> n;
+        std::cout <<fibonacci_fast(n);
+    return 0;
+}
